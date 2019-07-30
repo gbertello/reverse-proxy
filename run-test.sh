a6e7340bddf974b3e98a6e947959b483a780b0a5
Guillaume BERTELLO <guillaume.bertello@amadeus.com>
2019-07-30 08:00:09 +0200

    Improve management of proxy conf files

--- run-test.sh
+++ run-test.sh
@@ -1,3 +1,8 @@
+if [ ! -e nginx/proxy_test.conf ]; then
+  echo "Create nginx/proxy_test.conf file to configure reverse proxy on test environment."
+  exit 1
+fi
+
 cp nginx/proxy_test.conf nginx/proxy.conf
 
 docker-compose -f docker-compose-test.yml down
--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
+if [ ! -e nginx/proxy_prod.conf ]; then
+  echo "Create nginx/proxy_prod.conf file to configure reverse proxy on production environment."
+  exit 1
+fi
+
 cp nginx/proxy_prod.conf nginx/proxy.conf
 
 docker-compose down
