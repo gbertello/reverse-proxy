72ee05907ad77dbd8bf5a6a42e4b92a9675845bc
Guillaume Bertello <guillaume.bertello@gmail.com>
2020-05-05 16:05:22 +0200

    Fix ports

--- nginx/start.sh
+++ nginx/start.sh
@@ -19,6 +19,7 @@ IMAGE=${PARENT##*/}_${CWD##*/}_${SYSTEM}
 stop -i $IMAGE &> /dev/null || true
 
 OPTIONS=""
+OPTIONS="$OPTIONS -p 80:80"
 
 if [[ $ENV -eq "prod" ]]
 then
@@ -27,8 +28,10 @@ then
   TARGET_VOLUME="/certs/"
   OPTIONS="$OPTIONS -v $VOLUME:$TARGET_VOLUME"
   mkdir -p $VOLUME
+  OPTIONS="$OPTIONS -p 443:443"
 fi
 
+
 cp $CWD/config/proxy_$SYSTEM.conf $CWD/config/proxy.conf
-start -i $IMAGE -s $SYSTEM -n $NETWORK -p 3000:80 $OPTIONS
+start -i $IMAGE -s $SYSTEM -n $NETWORK $OPTIONS
 rm $CWD/config/proxy.conf
